cd753de7908f46e7da4ffe97171dfb9570f13fbd
Juan Francisco Gonzalez <jf.gonzalez@globant.com>
2020-07-30 15:49:59 -0500

    Add book interface & create-book

--- src/app/books/list-book/list-book.component.ts
+++ src/app/books/list-book/list-book.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { IBook } from "./../book";
 @Component({
   selector: 'app-list-book',
   templateUrl: './list-book.component.html',
   styleUrls: ['./list-book.component.scss']
 })
 export class ListBookComponent implements OnInit {
+  bookList: IBook[] = [];
 
   constructor() { }
 
