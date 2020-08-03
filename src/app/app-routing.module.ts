5f2f10192c2e7a7470c5650e4ed65c529d41251d
Juan Francisco Gonzalez <jf.gonzalez@globant.com>
2020-08-02 20:19:05 -0500

    Add card component & working routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateBookComponent } from "./books/create-book/create-book.component";
+import { ListBookComponent } from "./books/list-book/list-book.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListBookComponent },
+  { path: 'create', component: CreateBookComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
